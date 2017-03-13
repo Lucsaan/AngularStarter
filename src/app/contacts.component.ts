bfaa30408ecfe6214623ef2ed3e3c045e7b454de
Andreas Schmieder <aschmieder@freicon.de>
2017-03-13 14:22:03 +0100

    NgOnInit() eingebaut

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './models/contact';
+import { CONTACT_DATA} from './data/contact-data';
+import { ContactsService} from './contacts.service';
+
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
+  
 })
 export class ContactsAppComponent {
+  
+  contacts: Contact[];
+  
   title = 'Angular 2 Master Class setup works!';
+  
+  constructor( private contactsService: ContactsService){}
+
+  ngOnInit(){
+    this.contacts = this.contactsService.getContacts();
+  }
+  
+trackByContactId(index, contact){
+  return contact.id;
+}
+  
 }
